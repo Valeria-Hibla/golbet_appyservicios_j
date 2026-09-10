e6e8ba407550bc53ee15e406e4d25a5e9161f17a
Valeria-Hibla <valeriarodriguez1127989@correo.itm.edu.co>
2026-09-10 18:12:08 -0500

    AditableEntity was created

---
+++ GolBet.Entities/Common/AuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace GolBet.Entities.Common;
+
+public abstract class AuditableEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+    public bool IsActive { get; set; } = true;
+
+}
