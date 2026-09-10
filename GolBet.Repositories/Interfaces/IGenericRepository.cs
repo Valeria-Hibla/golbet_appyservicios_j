0144ea0b42310c3d02ce8cc95dde2620d356aca8
Valeria-Hibla <valeriarodriguez1127989@correo.itm.edu.co>
2026-09-10 18:45:21 -0500

    IGenericRepository was created

---
+++ GolBet.Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using GolBet.Entities.Common;
+
+namespace GolBet.Repositories.Interfaces;
+
+public interface IGenericRepository<T> where T : AuditableEntity
+
+{
+
+    Task<IEnumerable<T>> GetAllAsync(bool includeInactive = false);
+
+    Task<T?> GetByIdAsync(int id);
+
+    Task<T> AddAsync(T entity);
+
+    Task UpdateAsync(T entity);
+
+    Task DeactivateAsync(int id);   
+
+}
